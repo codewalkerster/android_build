69965a90d59452296b786a1cda9d25ce83aad4d8
Frank Chen <frank.chen@amlogic.com>
2015-06-30 09:44:36 +0800

    support BOARD_KERNEL_OFFSET for boot.img
    Change-Id: I1818c350e89cd8298000537d426fa06877b1ac57

--- tools/releasetools/common.py
+++ tools/releasetools/common.py
@@ -333,6 +333,11 @@ def BuildBootableImage(sourcedir, fs_config_file, info_dict=None):
     cmd.append("--pagesize")
     cmd.append(open(fn).read().rstrip("\n"))
 
+  fn = os.path.join(sourcedir, "kernel_offset")
+  if os.access(fn, os.F_OK):
+    cmd.append("--kernel_offset")
+    cmd.append(open(fn).read().rstrip("\n"))
+
   args = info_dict.get("mkbootimg_args", None)
   if args and args.strip():
     cmd.extend(shlex.split(args))
