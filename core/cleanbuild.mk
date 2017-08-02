02d767b7dfeb055a741693d5d6743a48e67979e8
Steven Chen <cw@rock-chips.com>
2017-08-02 20:04:30 +0800

    rk3288: Add dual vendor support
    relevant repos(change id):
    build:I80af33aaad1fb303f1b6abbda4e010c9cbd8cdae
    device/rockchip/common:I081de9109b2b3f91b232671458507343dcd019d0
    device/rockchip/rk3288:I4629a2d8196543eae2d144ad3ddc72826762199e
    hardware/broadcom/libbt:Idf15ff6883371da58001320be64a375be900d397
    system/core:Iee2187c3c1938d330a317016d7048a958b6e714e
    vendor/widevine:Ib60beccd5edc983fa5232550b8379934d9cd2849
    vendor/rockchip/common:If98b108ddf7a1228bd5c0e5fe64ae688a3e5eb7c
    RKTools:I9c0b62294045f5a908096e70b2acadeae3fb49bd
    
    Change-Id: I80af33aaad1fb303f1b6abbda4e010c9cbd8cdae
    Signed-off-by: Steven Chen <cw@rock-chips.com>

--- core/cleanbuild.mk
+++ core/cleanbuild.mk
@@ -244,6 +244,8 @@ installclean_files := \
 	$(PRODUCT_OUT)/system \
 	$(PRODUCT_OUT)/system_other \
 	$(PRODUCT_OUT)/vendor \
+	$(PRODUCT_OUT)/vendor0 \
+	$(PRODUCT_OUT)/vendor1 \
 	$(PRODUCT_OUT)/oem \
 	$(PRODUCT_OUT)/dex_bootjars \
 	$(PRODUCT_OUT)/obj/JAVA_LIBRARIES \
