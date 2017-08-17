9d2ddd5e41df3061515979cecccd5ce8bf967f7d
Luke Go <luke.go@hardkernel.com>
2017-08-17 14:03:48 +0900

    Revert "Modify Build Number for eng && user"
    This reverts commit 81b43bf79711c0e3e31a49d8f74cc24abecc975f.
    
    Change-Id: Id4841f0ae677b73ac5ab8058a38ae4ea024f8c7f

--- core/version_defaults.mk
+++ core/version_defaults.mk
@@ -173,8 +173,5 @@ ifeq "" "$(BUILD_NUMBER)"
   # If no BUILD_NUMBER is set, create a useful "I am an engineering build
   # from this date/time" value.  Make it start with a non-digit so that
   # anyone trying to parse it as an integer will probably get "0".
-  BUILD_NUMBER := eng.$(USER).$(shell date +%Y%m%d.%H%M%S)
-ifeq ($(TARGET_BUILD_VARIANT),user)
   BUILD_NUMBER := user.$(USER).$(shell date +%Y%m%d.%H%M%S)
-endif
 endif
