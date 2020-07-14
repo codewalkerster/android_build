6a4d1fd0bdf453c4ee552f60961fcddb46b95bb5
Bian Jin chen <kenjc.bian@rock-chips.com>
2020-07-14 16:31:13 +0800

    envsetup: export TARGET_BOARD_PLATFORM/TARGET_BOARD_PLATFORM_GPU.
    Signed-off-by: Bian Jin chen <kenjc.bian@rock-chips.com>
    Change-Id: Ic0cab3001cb3744a328b4e783b9800dc8cd302e6

--- envsetup.sh
+++ envsetup.sh
@@ -314,6 +314,12 @@ function setpaths()
     unset ANDROID_TARGET_OUT_TESTCASES
     export ANDROID_TARGET_OUT_TESTCASES=$(get_abs_build_var TARGET_OUT_TESTCASES)
 
+    unset TARGET_BOARD_PLATFORM_GPU
+    export TARGET_BOARD_PLATFORM_GPU=$(get_build_var TARGET_BOARD_PLATFORM_GPU)
+
+    unset TARGET_BOARD_PLATFORM
+    export TARGET_BOARD_PLATFORM=$(get_build_var TARGET_BOARD_PLATFORM)
+
     # needed for building linux on MacOS
     # TODO: fix the path
     #export HOST_EXTRACFLAGS="-I "$T/system/kernel_headers/host_include
