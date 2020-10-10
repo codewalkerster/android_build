3b2338b115e7c817d920627a11c1f350c99d8933
Dayao Ji <jdy@rock-chips.com>
2020-10-10 16:45:09 +0800

    Support pack resource.img according to booot header version
    Signed-off-by: Dayao Ji <jdy@rock-chips.com>
    Change-Id: I21c237d2bd3223c98366df9caee3e26a1899bfd8

--- tools/releasetools/common.py
+++ tools/releasetools/common.py
@@ -1216,7 +1216,14 @@ def _BuildBootableImage(image_name, sourcedir, fs_config_file, info_dict=None,
 
   cmd = [mkbootimg, "--kernel", os.path.join(sourcedir, kernel)]
 
-  fn = os.path.join(sourcedir, "second")
+  if partition_name == "recovery":
+    fn = os.path.join(sourcedir, "resource.img")
+  else:
+    header_version = info_dict.get("booot_header_version")
+    if header_version < 3:
+      fn = os.path.join(sourcedir, "resource.img")
+    else:
+      fn = os.path.join(sourcedir, "second")
   if os.access(fn, os.F_OK):
     cmd.append("--second")
     cmd.append(fn)
